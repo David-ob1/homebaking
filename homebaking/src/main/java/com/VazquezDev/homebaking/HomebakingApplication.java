5590dd82bb6e380c8da9d3f873e3ca906992a80c
David-ob1 <davidvazquezt2001@gmail.com>
2024-04-01 18:27:07 -0300

    initial

---
+++ homebaking/src/main/java/com/VazquezDev/homebaking/HomebakingApplication.java
@@ -0,0 +1,13 @@
+package com.VazquezDev.homebaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebakingApplication.class, args);
+	}
+
+}
---
+++ homebaking/src/test/java/com/VazquezDev/homebaking/HomebakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VazquezDev.homebaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
