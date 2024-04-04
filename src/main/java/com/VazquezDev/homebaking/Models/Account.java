0a324ade8821ccc925022bfd8d9682f0f8594702
David-ob1 <davidvazquezt2001@gmail.com>
2024-04-03 22:47:54 -0300

    crear clase account

---
+++ src/main/java/com/VazquezDev/homebaking/Models/Account.java
@@ -0,0 +1,61 @@
+package com.VazquezDev.homebaking.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String number;
+    private LocalDate creationDate;
+
+    private double balance;
+
+
+    public Account() {
+    }
+
+    public Account(String number, LocalDate creationDate, double balance) {
+        this.number = number;
+        this.creationDate = creationDate;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
+
+
