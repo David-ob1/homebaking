5ea527965cbba6103eadef76bc08dcfc07f2bab7
David-ob1 <davidvazquezt2001@gmail.com>
2024-04-06 16:50:06 -0300

    efecto en login

--- src/main/resources/static/web/script/login.js
+++ src/main/resources/static/web/script/login.js
@@ -1,48 +1,75 @@
- const formRegister = document.getElementById("register")
-
- let registerEP = "/api/client/new"
-
- let $name = document.getElementById("name")
- let lastName = document.getElementById("lastName")
- let email = document.getElementById("email")
- let password = document.getElementById("password")
+const  wrapper = document.querySelector(".wrapper")
+const  registerLink = document.querySelector(".register-link")
+const  loginLink = document.querySelector(".login-link")
 
 
-//  console.log()
- const btnRegister = document.getElementById("btn-register")
-//  alert("hola")
-    btnRegister.addEventListener("click",()=>{
-
-        let newClient = obtenerValores()
-
-        axios.post(registerEP, newClient)
-            .then(data =>{
-                console.log(data)
-
-                setTimeout(() => {
-                    location.href = '../index.html';
-                    console.log("Retrasado por 2 segundo.");
-                  }, 2000);
+registerLink.onclick = ()=>{
+    wrapper.classList.add("active")
+}
 
 
 
-            })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  const formRegister = document.getElementById("register")
+
+//  let registerEP = "/api/client/new"
+
+//  let $name = document.getElementById("name")
+//  let lastName = document.getElementById("lastName")
+//  let email = document.getElementById("email")
+//  let password = document.getElementById("password")
+
+
+// //  console.log()
+//  const btnRegister = document.getElementById("btn-register")
+// //  alert("hola")
+//     btnRegister.addEventListener("click",()=>{
+
+//         let newClient = obtenerValores()
+
+//         axios.post(registerEP, newClient)
+//             .then(data =>{
+//                 console.log(data)
+
+//                 setTimeout(() => {
+//                     location.href = '../index.html';
+//                     console.log("Retrasado por 2 segundo.");
+//                   }, 2000);
+
+
+
+//             })
 
             
-            .catch(error => console.log(error))
+//             .catch(error => console.log(error))
 
 
         
-    })
+//     })
 
 
  
-  function obtenerValores(){
-       return {
-        name: $name.value,
-        lastName: lastName.value,
-        email: email.value,
-        password: password.value
-    };
+//   function obtenerValores(){
+//        return {
+//         name: $name.value,
+//         lastName: lastName.value,
+//         email: email.value,
+//         password: password.value
+//     };
 
-    }
+//     }
